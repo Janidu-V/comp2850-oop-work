495880e558ad55aa6be49b71f28625937c382cce
Janidu-V <njhr4836@leeds.ac.uk>
2025-10-02 23:16:22 +0000

    tasks 4 done

---
+++ tasks/task4_4/Pizza.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    do {
+        print("Choose pizza (a,b,c,d): ")
+        val userInput = readln().lowercase()
+
+        if (userInput.length == 1 && userInput in "a".."d") {
+            println("Order accepted")
+        } else {
+            println("Invalid option")
+        }
+
+    } while (userInput.length != 1 || userInput !in "a".."d")
+}
--- tasks/task4_5/OddSum.kt
+++ tasks/task4_5/OddSum.kt
@@ -1,1 +1,12 @@
 // Task 4.5: summing odd integers with a for loop
+fun main() {
+    print("Enter the number: ")
+    val upper = readln().toLong()
+    var total = 0L
+
+    for (i in 1..(upper)) {
+        total += i
+    }
+
+    println(total)
+}
