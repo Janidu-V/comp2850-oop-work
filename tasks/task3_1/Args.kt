e1977783689af66d6112b41a8bfb6c679f975916
Janidu-V <njhr4836@leeds.ac.uk>
2025-10-02 15:50:47 +0000

    Tasks 3.1, 3.2 done

--- tasks/task3_1/Args.kt
+++ tasks/task3_1/Args.kt
@@ -1,1 +1,13 @@
+import kotlin.system.exitProcess
+
+
 // Task 3.1: command line arguments
+fun main(args: Array<String>) {
+    if (args.size != 2) {
+        println("Invalid arguments")
+        exitProcess(1)
+    }
+    
+    println(args[0])
+    println(args[1])
+}
